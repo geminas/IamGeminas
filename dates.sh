80a692c99d7f2f188c0a9c2fe5b960d60482f099
geminas <geminas@aliyun.com>
2015-10-15 21:30:02 +0800

    hello geminas

---
+++ dates.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+while read date
+do
+    fileName=`echo "$date" | tr " " "_"`
+    date="$date 14:00 2015 +0500"
+    echo "Creating file... $fileName"
+    touch "$fileName"
+    git add "$fileName"
+    git commit --date="$date" --author="geminas" -m "$fileName"
+done <dates.txt
